32831d2570721fd06014d45994d53cef339c67bf
Thibault Piana <astres3000@free.fr>
2015-10-20 20:07:11 +0200

    Begin to code GPIO driver

---
+++ drivers/gpio/driver_gpio.c
@@ -0,0 +1,5 @@
+//
+// Created by thibault on 20/10/15.
+//
+
+#include "driver_gpio.h"
---
+++ drivers/gpio/driver_gpio.h
@@ -0,0 +1,8 @@
+//
+// Created by thibault on 20/10/15.
+//
+
+#ifndef ONLYOS_DRIVER_GPIO_H
+#define ONLYOS_DRIVER_GPIO_H
+
+#endif //ONLYOS_DRIVER_GPIO_H
