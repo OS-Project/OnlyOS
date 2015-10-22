6825af784816553f51e363751f8233d272f13322
Thibault Piana <astres3000@free.fr>
2015-10-22 20:36:29 +0200

    Rearrane directories

--- drivers/gpio/driver_gpio.c
+++ drivers/gpio/driver_gpio.c
@@ -12,37 +12,38 @@
 #include "gpio_v2.h"
 #include "utils_errno.h"
 
-void GPIO_setPin(unsigned int base_gpio, unsigned int pin, unsigned int state)
+void GPIO_setPin(unsigned int gpio_base, unsigned int pin, unsigned int state)
 {
-    unsigned int base_adress = GPIO_getBaseAdress(base_gpio);
+    unsigned int gpio_base_adress = GPIO_getBaseAdress(base_gpio);
 
     switch(state)
     {
         case GPIO_PIN_MODE_INPUT:
-            GPIODirModeSet(base_adress,
-                           pin,
-                           GPIO_DIR_INPUT);
+            GPIODirModeSet(gpio_base_adress, gpio_pin, GPIO_DIR_INPUT);
         case GPIO_PIN_MODE_OUTPUT:
+            GPIODirModeSet(gpio_base_adress, gpio_pin, GPIO_DIR_OUTPUT);
         case GPIO_PIN_STATE_HIGH:
+            GPIOPinWrite(gpio_base_adress, gpio_pin, GPIO_PIN_LOW);
         case GPIO_PIN_STATE_LOW:
+            GPIOPinWrite(gpio_base_adress, gpio_pin, GPIO_PIN_HIGH);
     }
 }
 
-void GPIO_enable(unsigned int base_gpio)
+void GPIO_enable(unsigned int gpio_base)
 {
-    unsigned int base_adress = GPIO_getBaseAdress(base_gpio);
+    unsigned int base_adress = GPIO_getBaseAdress(gpio_base);
     GPIOModuleEnable(base_adress);
 }
 
-void GPIO_reset(unsigned int base_gpio)
+void GPIO_reset(unsigned int gpio_base)
 {
-    unsigned int base_adress = GPIO_getBaseAdress(base_gpio);
+    unsigned int base_adress = GPIO_getBaseAdress(gpio_base);
     GPIOModuleReset(base_adress);
 }
 
-void GPIO_getBaseAdress(unsigned int base_gpio)
+unsigned int GPIO_getBaseAdress(unsigned int gpio_base)
 {
-    switch(base_gpio)
+    switch(gpio_base)
     {
         case 0:
             unsigned int base_adress = SOC_GPIO_0_REGS;
@@ -55,5 +56,5 @@ void GPIO_getBaseAdress(unsigned int base_gpio)
         default:
             error("GPIO invalide pour la beaglebone");
     }
-    return base_adress;
+    return gpio_base_adress;
 }
