4ad7ff8516bd9f358841143b36122c7fb68bd951
Thibault PIANA <astres3000@free.fr>
2015-12-27 16:34:03 +0100

    Tests

--- drivers/tests/uart/uart.c
+++ drivers/tests/uart/uart.c
@@ -3,10 +3,11 @@
 //
 
 #include "drivers/uart/uart.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+    int i;
     UART_writeLn("Ceci est un test de la fonction malloc");
     UART_writeLn("On va ajouter plusieurs espaces mémoires a la suite pour voir ce que ça fait");
 
--- syscall/syscall.c
+++ syscall/syscall.c
@@ -31,16 +31,16 @@ int _read(int file, char *ptr, int len) {
 
 char *heap_end = 0;
 caddr_t _sbrk(int incr) {
-    extern char _sheap; /* Defined by the linker */
-    extern char _eheap; /* Defined by the linker */
+    extern char heap_low; /* Defined by the linker */
+    extern char heap_top; /* Defined by the linker */
     char *prev_heap_end;
 
     if (heap_end == 0) {
-        heap_end = &_sheap;
+        heap_end = &heap_low;
     }
     prev_heap_end = heap_end;
 
-    if (heap_end + incr > &_eheap) {
+    if (heap_end + incr > &heap_top) {
         /* Heap and stack collision */
         return (caddr_t)0;
     }
