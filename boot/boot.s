8d20016433a53210104556416c6d51e79d9c8b13
Alan Gardin <alan.gardin@protonmail.com>
2016-05-12 09:27:58 +0200

    Added irq, fiq

--- boot/boot.s
+++ boot/boot.s
@@ -1,13 +1,66 @@
 .section ".text.interrupt_vector_table"
 interrupt_vector_table:
     b _start // Reset Handler
-    b . // Undefined instruction
-    b swi_handler // SWI Handler (Software interrupt)
-    b . // Prefetch Abort
-    b . // Data Abort
-    b . // Reserved
-    b irq_handler // IRQ
-    b . // FIQ
+    b error // Undefined instruction
+    b svc_handler // SVC Handler (Software interrupt)
+    b error // Prefetch Abort
+    b error // Data Abort
+    b error // Reserved
+    b error // IRQ
+	b error
+	nop // FIQ
+
+.section ".text.interrupt_handler"
+fiq_handler:
+        // Done auto: spsr = cpsr. Ref: p456
+        stmfd sp!, {r0-r12,lr} // Save registers
+        mrs r11, spsr // Save context
+
+        // r0 contains the interrupt id
+        ldr r0, =0x48200044 // Ref: p183 (Interrupt controller), p470
+        and r0, r0, #0b1111111 // Ref: p476
+
+        // Branch table
+
+        // Interrupt has been taken care of. Enable new interrupts
+        mov r0, #0x2
+        ldr r1, =0x48200048 // Ref: p457
+        str r0, [r1]
+        dsb // Data synchronization barrier. Ref: p458
+
+        msr cpsr, r11 // Restore context
+        ldmfd sp!, {r0-r12, lr} // Restore the saved registers from the stack
+        subs pc, lr, #4
+        //Done auto: cpsr = spsr; pc=lr. Ref: p458
+
+irq_handler:
+        // Done auto: spsr = cpsr. Ref: p456
+        stmfd sp!, {r0-r12,lr} // Save registers
+        mrs r11, spsr // Save context
+
+        // r0 contains the interrupt id
+        ldr r0, =0x48200040 // Ref: p183 (Interrupt controller), p470
+        and r0, r0, #0b1111111 // Ref: p475
+
+        // Branch table
+
+        // Interrupt has been taken care of. Enable new interrupts
+        mov r0, #0x1
+        ldr r1, =0x48200048 // Ref: p457
+        str r0, [r1]
+        dsb // Data synchronization barrier. Ref: p458
+
+        msr cpsr, r11 // Restore context
+        ldmfd sp!, {r0-r12, lr} // Restore the saved registers from the stack
+        subs pc, lr, #4
+        //Done auto: cpsr = spsr; pc=lr. Ref: p458
+
+svc_handler:
+        mrs r11, cpsr
+
+        msr cpsr, r11
+        movs pc,lr
+
 
 
 .section ".text.boot"
@@ -29,13 +82,11 @@ _start:
         mov	sp, r0
 
         msr cpsr, #irq_mode
-        ldr r0, =_e_svc_stack
+        ldr r0, =_e_irq_stack
         mov sp, r0
 
 
-        //msr cpsr, #und_mode
-        //msr cpsr, #abort_mode
-        //msr cpsr, #fiq_mode
+        // Kernel mode
         msr cpsr, #sys_mode
         ldr r0, =_e_sys_stack
         mov sp, r0
@@ -59,8 +110,11 @@ _start:
         b	kmain
 
 
-swi_handler:
-irq_handler:
+error:
+	mov r0, #1
+	b kexit
+
 halt:
-    mov r0, #0 // No error
+	// TODO: Remove interrupts
+	mov r0, #0 // No error
     b	kexit
