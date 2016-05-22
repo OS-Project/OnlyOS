7e5ee166f927182ad0aa51b4adb7a586b01debc3
Alan Gardin <alan.gardin@protonmail.com>
2016-05-22 19:02:20 +0200

    INt_SVC_handler completed

--- include/kernel/interrupt.h
+++ include/kernel/interrupt.h
@@ -1,6 +1,6 @@
 #ifndef ONLYOS_INTERRUPT_H
 #define ONLYOS_INTERRUPT_H
-	void INT_SVC_handler();
+	void INT_SVC_handler(int r0, int r1, int r2, int r3);
 	void INT_SVC_call(int r0, int r1, int r2, int r3);
 	extern void svc_asm_call(int r0, int r1, int r2, int r3);
 #endif
--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -1,10 +1,17 @@
 #include "kernel/interrupt.h"
 
-void INT_SVC_handler()
+void INT_SVC_handler(int r0, int r1, int r2, int r3)
 {
     #ifdef DEBUG
-        kprintf("\nSVC interrupt detected\n");
+        kprintf("SVC interrupt detected\n");
+	kprintf("SVC arguments: r0=%x, r1=%x, r2=%x, r3=%x\n", r0, r1, r2, r3);
     #endif
+	switch (r0)
+	{
+		default:
+			kprintf("Unknown SVC call\n");
+			break;
+	}
 }
 
 void INT_SVC_call(int r0, int r1, int r2, int r3)
