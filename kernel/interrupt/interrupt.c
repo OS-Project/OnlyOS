3d6c49a0a172fe7ca4b353f3099a89bdebdb3339
Thibault Piana <astres3000@free.fr>
2016-06-11 14:05:53 +0200

    Delete useless files

--- kernel/interrupt/interrupt.c
+++ kernel/interrupt/interrupt.c
@@ -1,27 +1,48 @@
 #include "drivers/uart/uart.h"
 #include "kernel/config.h"
 #include "kernel/interrupt/interrupt.h"
+#include <kernel/kernel.h>
 
+
+/* Memory management */
+#include <kernel/memory/memory.h>
+/**
+ *
+ */
 void INT_SVC_handler(int r0, int r1, int r2, int r3)
 {
     #ifdef DEBUG
         kprintf("SVC interrupt detected\n");
 	kprintf("SVC arguments: r0=%x, r1=%x, r2=%x, r3=%x\n", r0, r1, r2, r3);
     #endif
+
 	switch (r0)
 	{
 		case 0:
 			switch(r1)
 			{
+				case 0x0:
+					kprintf("\nFatal Error : System aborted");
+					kexit();
+					break;
 				default:
 					kprintf("Error exception: error number unknown.\n");
 					break;
 			}
+
 			break;
 
-		case 100:
-			UART_putc(r1);
+		case 0x00100:
+			UART_putc((unsigned char)r1);
 			break;
+
+		case 0x00200:
+			/* minit */
+			break;
+
+		case 0x00201:
+			mmalloc(r1, mget_memory(r2));
+
 
 		default:
 			kprintf("Unknown SVC call\n");
@@ -36,7 +57,7 @@ void INT_SVC_call(int r0, int r1, int r2, int r3)
 
 void INT_IRQ_handler()
 {
-	#ifdef DEBUG
+	#ifdef DEBUG_INTERRUPT
 		kprintf("IRQ interrupt detected\n");
 	#endif
 	int * INTC_SIR_IRQ = (int *)(0x48200040);
